0e69a3bbed47474ffb45f50b975d7ebda9d27ab8
Sandra Cabello <xandra.cj@gmail.com>
2024-06-28 21:54:29 +0200

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://sandracabello.github.io/portfolio/',
+  site: 'https://sandracabello.github.io',
   output:'static',
   base: process.env.PUBLIC_BASE_URL || '/',
 });
